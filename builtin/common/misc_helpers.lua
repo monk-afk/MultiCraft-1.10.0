monk-afk/MultiCraft-1.10.0

commit befd5e32da0e963467ca100f0255b5c5eb042248

Add multiline support to colorize. (#5444)

builtin/common/misc_helpers.lua
@@ -667,7 +667,14 @@ else
 	end
 
 	function core.colorize(color, message)
-		return core.get_color_escape_sequence(color) .. message .. core.get_color_escape_sequence("#ffffff")
+		local lines = message:split("\n", true)
+		local color_code = core.get_color_escape_sequence(color)
+
+		for i, line in ipairs(lines) do
+			lines[i] = colour_code .. line
+		end
+
+		return table.concat(lines, "\n") .. core.get_color_escape_sequence("#ffffff")
 	end
 
 end

commit 7915c7107ed5e04c79f6f25a41d922a1932fdc6c

Fix a type bug in colorize function
This bug was introduced by befd5e32da0e963467ca100f0255b5c5eb042248 and reported by @kilbith

builtin/common/misc_helpers.lua
@@ -667,7 +667,7 @@ else
 	end
 
 	function core.colorize(color, message)
-		local lines = message:split("\n", true)
+		local lines = tostring(message):split("\n", true)
 		local color_code = core.get_color_escape_sequence(color)
 
 		for i, line in ipairs(lines) do