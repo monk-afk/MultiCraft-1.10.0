monk-afk/MultiCraft-1.10.0

commit 42d2a36c9a70923fd24fbfcd3d9fb591d0ad7e16

[CSM] Add event on_place_node API lua (#5548)
* [CSM] Add event on_place_node API lua

src/script/common/c_content.cpp
@@ -32,6 +32,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "porting.h"
 #include "mg_schematic.h"
 #include "noise.h"
+#include "util/pointedthing.h"
 #include <json/json.h>
 
 struct EnumString es_TileAnimationType[] =
@@ -115,6 +116,16 @@ void read_item_definition(lua_State* L, int index,
 	// "" = no prediction
 	getstringfield(L, index, "node_placement_prediction",
 			def.node_placement_prediction);
+}
+
+/******************************************************************************/
+void push_item_definition(lua_State *L, const ItemDefinition &i)
+{
+	lua_newtable(L);
+	lua_pushstring(L, i.name.c_str());
+	lua_setfield(L, -2, "name");
+	lua_pushstring(L, i.description.c_str());
+	lua_setfield(L, -2, "description");
 }
 
 /******************************************************************************/
@@ -1426,4 +1437,37 @@ void read_json_value(lua_State *L, Json::Value &root, int index, u8 recursion)
 		throw SerializationError("Can only store booleans, numbers, strings, objects, arrays, and null in JSON");
 	}
 	lua_pop(L, 1); // Pop value
+}
+
+void push_pointed_thing(lua_State *L, const PointedThing &pointed)
+{
+	lua_newtable(L);
+	if (pointed.type == POINTEDTHING_NODE) {
+		lua_pushstring(L, "node");
+		lua_setfield(L, -2, "type");
+		push_v3s16(L, pointed.node_undersurface);
+		lua_setfield(L, -2, "under");
+		push_v3s16(L, pointed.node_abovesurface);
+		lua_setfield(L, -2, "above");
+	} else if (pointed.type == POINTEDTHING_OBJECT) {
+		lua_pushstring(L, "object");
+		lua_setfield(L, -2, "type");
+		push_objectRef(L, pointed.object_id);
+		lua_setfield(L, -2, "ref");
+	} else {
+		lua_pushstring(L, "nothing");
+		lua_setfield(L, -2, "type");
+	}
+}
+
+void push_objectRef(lua_State *L, const u16 id)
+{
+	// Get core.object_refs[i]
+	lua_getglobal(L, "core");
+	lua_getfield(L, -1, "object_refs");
+	luaL_checktype(L, -1, LUA_TTABLE);
+	lua_pushnumber(L, id);
+	lua_gettable(L, -2);
+	lua_remove(L, -2); // object_refs
+	lua_remove(L, -2); // core
 }

src/script/common/c_content.h
@@ -88,6 +88,8 @@ void               push_tool_capabilities    (lua_State *L,
 
 void read_item_definition (lua_State *L, int index, const ItemDefinition &default_def,
 		ItemDefinition &def);
+void               push_item_definition      (lua_State *L,
+                                              const ItemDefinition &i);
 void               read_object_properties    (lua_State *L, int index,
                                               ObjectProperties *prop,
                                               IItemDefManager *idef);
@@ -161,6 +163,10 @@ bool               push_json_value           (lua_State *L,
                                               int nullindex);
 void               read_json_value           (lua_State *L, Json::Value &root,
                                               int index, u8 recursion = 0);
+
+void               push_pointed_thing        (lua_State *L, const PointedThing &pointed);
+
+void               push_objectRef            (lua_State *L, const u16 id);
 
 extern struct EnumString es_TileAnimationType[];
 

src/script/cpp_api/s_client.cpp
@@ -23,6 +23,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "client.h"
 #include "common/c_converter.h"
 #include "common/c_content.h"
+#include "s_item.h"
 
 void ScriptApiClient::on_shutdown()
 {
@@ -187,6 +188,23 @@ bool ScriptApiClient::on_punchnode(v3s16 p, MapNode node)
 	runCallbacks(2, RUN_CALLBACKS_MODE_OR);
 	bool blocked = lua_toboolean(L, -1);
 	return blocked;
+}
+
+bool ScriptApiClient::on_placenode(const PointedThing &pointed, const ItemDefinition &item)
+{
+	SCRIPTAPI_PRECHECKHEADER
+
+	// Get core.registered_on_placenode
+	lua_getglobal(L, "core");
+	lua_getfield(L, -1, "registered_on_placenode");
+
+	// Push data
+	push_pointed_thing(L, pointed);
+	push_item_definition(L, item);
+
+	// Call functions
+	runCallbacks(2, RUN_CALLBACKS_MODE_OR);
+	return lua_toboolean(L, -1);
 }
 
 void ScriptApiClient::setEnv(ClientEnvironment *env)

builtin/client/register.lua
@@ -69,3 +69,4 @@ core.registered_on_damage_taken, core.register_on_damage_taken = make_registrati
 core.registered_on_formspec_input, core.register_on_formspec_input = make_registration()
 core.registered_on_dignode, core.register_on_dignode = make_registration()
 core.registered_on_punchnode, core.register_on_punchnode = make_registration()
+core.registered_on_placenode, core.register_on_placenode = make_registration()

clientmods/preview/init.lua
@@ -10,6 +10,13 @@ core.register_on_connect(function()
 	print("[PREVIEW] Player connection completed")
 end)
 
+core.register_on_placenode(function(pointed_thing, node)
+	print("The local player place a node!")
+	print("pointed_thing :" .. dump(pointed_thing))
+	print("node placed :" .. dump(node))
+	return false
+end)
+
 -- This is an example function to ensure it's working properly, should be removed before merge
 core.register_on_receiving_chat_messages(function(message)
 	print("[PREVIEW] Received message " .. message)

src/game.cpp
@@ -3730,6 +3730,9 @@ void Game::handlePointingAtNode(const PointedThing &pointed, const ItemDefinitio
 				// Read the sound
 				soundmaker->m_player_rightpunch_sound =
 						playeritem_def.sound_place;
+
+				if (client->moddingEnabled())
+					client->getScript()->on_placenode(pointed, playeritem_def);
 			} else {
 				soundmaker->m_player_rightpunch_sound =
 						SimpleSoundSpec();

src/script/cpp_api/s_base.cpp
@@ -42,6 +42,7 @@ extern "C" {
 
 #include <stdio.h>
 #include <cstdarg>
+#include "script/common/c_content.h"
 #include <sstream>
 
 
@@ -320,20 +321,8 @@ void ScriptApiBase::objectrefGetOrCreate(lua_State *L,
 	if (cobj == NULL || cobj->getId() == 0) {
 		ObjectRef::create(L, cobj);
 	} else {
-		objectrefGet(L, cobj->getId());
+		push_objectRef(L, cobj->getId());
 	}
-}
-
-void ScriptApiBase::objectrefGet(lua_State *L, u16 id)
-{
-	// Get core.object_refs[i]
-	lua_getglobal(L, "core");
-	lua_getfield(L, -1, "object_refs");
-	luaL_checktype(L, -1, LUA_TTABLE);
-	lua_pushnumber(L, id);
-	lua_gettable(L, -2);
-	lua_remove(L, -2); // object_refs
-	lua_remove(L, -2); // core
 }
 
 Server* ScriptApiBase::getServer()

src/script/cpp_api/s_base.h
@@ -115,7 +115,6 @@ protected:
 	void setGuiEngine(GUIEngine* guiengine) { m_guiengine = guiengine; }
 
 	void objectrefGetOrCreate(lua_State *L, ServerActiveObject *cobj);
-	void objectrefGet(lua_State *L, u16 id);
 
 	RecursiveMutex  m_luastackmutex;
 	std::string     m_last_run_mod;

commit 6847cc3f8851a950d9f88f1a03a3b81e5dcab8d3

Fix CSM crash (#5779)
Caused by 42d2a36 and them made worse by 4abf7a1

src/script/common/c_content.cpp
@@ -1441,7 +1441,7 @@ void read_json_value(lua_State *L, Json::Value &root, int index, u8 recursion)
 	lua_pop(L, 1); // Pop value
 }
 
-void push_pointed_thing(lua_State *L, const PointedThing &pointed)
+void push_pointed_thing(lua_State *L, const PointedThing &pointed, bool csm)
 {
 	lua_newtable(L);
 	if (pointed.type == POINTEDTHING_NODE) {
@@ -1454,8 +1454,14 @@ void push_pointed_thing(lua_State *L, const PointedThing &pointed)
 	} else if (pointed.type == POINTEDTHING_OBJECT) {
 		lua_pushstring(L, "object");
 		lua_setfield(L, -2, "type");
-		push_objectRef(L, pointed.object_id);
-		lua_setfield(L, -2, "ref");
+
+		if (csm) {
+			lua_pushinteger(L, pointed.object_id);
+			lua_setfield(L, -2, "id");
+		} else {
+			push_objectRef(L, pointed.object_id);
+			lua_setfield(L, -2, "ref");
+		}
 	} else {
 		lua_pushstring(L, "nothing");
 		lua_setfield(L, -2, "type");

src/script/common/c_content.h
@@ -164,7 +164,7 @@ bool               push_json_value           (lua_State *L,
 void               read_json_value           (lua_State *L, Json::Value &root,
                                               int index, u8 recursion = 0);
 
-void               push_pointed_thing        (lua_State *L, const PointedThing &pointed);
+void               push_pointed_thing        (lua_State *L, const PointedThing &pointed, bool csm = false);
 
 void               push_objectRef            (lua_State *L, const u16 id);
 

src/script/cpp_api/s_client.cpp
@@ -199,7 +199,7 @@ bool ScriptApiClient::on_placenode(const PointedThing &pointed, const ItemDefini
 	lua_getfield(L, -1, "registered_on_placenode");
 
 	// Push data
-	push_pointed_thing(L, pointed);
+	push_pointed_thing(L, pointed, true);
 	push_item_definition(L, item);
 
 	// Call functions
@@ -217,7 +217,7 @@ bool ScriptApiClient::on_item_use(const ItemStack &item, const PointedThing &poi
 
 	// Push data
 	LuaItemStack::create(L, item);
-	push_pointed_thing(L, pointed);
+	push_pointed_thing(L, pointed, true);
 
 	// Call functions
 	runCallbacks(2, RUN_CALLBACKS_MODE_OR);