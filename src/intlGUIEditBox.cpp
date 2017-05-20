monk-afk/MultiCraft-1.10.0

commit 16a9ff1bbaeb79f104d6cd8acb88f8309d6825f4

Fix textarea bug: clicking right of the end of a line caused the cursor to be in one of the next lines (#3474)

src/intlGUIEditBox.cpp
@@ -1120,6 +1120,8 @@ s32 intlGUIEditBox::getCursorPos(s32 x, s32 y)
 
 	if (x < CurrentTextRect.UpperLeftCorner.X)
 		x = CurrentTextRect.UpperLeftCorner.X;
+	else if (x > CurrentTextRect.LowerRightCorner.X)
+		x = CurrentTextRect.LowerRightCorner.X;
 
 	s32 idx = font->getCharacterFromPos(Text.c_str(), x - CurrentTextRect.UpperLeftCorner.X);
 
@@ -1127,7 +1129,7 @@ s32 intlGUIEditBox::getCursorPos(s32 x, s32 y)
 	if (idx != -1)
 		return idx + startPos;
 
-	// click was off the right edge of the line, go to end.
+	// click was off the right edge of the last line, go to end.
 	return txtLine->size() + startPos;
 }
 

commit 43ba3688f4b5ae4fb7d2ad52ac2ce8c92e2b3153

Fix input regression introduced by 16a9ff1bbaeb79f104d6cd8acb88f8309d6825f4
Fix #5776

src/intlGUIEditBox.cpp
@@ -1120,8 +1120,8 @@ s32 intlGUIEditBox::getCursorPos(s32 x, s32 y)
 
 	if (x < CurrentTextRect.UpperLeftCorner.X)
 		x = CurrentTextRect.UpperLeftCorner.X;
-	else if (x > CurrentTextRect.LowerRightCorner.X)
-		x = CurrentTextRect.LowerRightCorner.X;
+	else if (x > CurrentTextRect.LowerRightCorner.X + 1)
+		x = CurrentTextRect.LowerRightCorner.X + 1;
 
 	s32 idx = font->getCharacterFromPos(Text.c_str(), x - CurrentTextRect.UpperLeftCorner.X);
 