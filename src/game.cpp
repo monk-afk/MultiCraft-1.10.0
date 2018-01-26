monk-afk/MultiCraft-1.10.0

commit 2181fc2e3d4a92bc91ee2e08757b164d819e3c02

Fix issues with earlier CSM HUD commit (#6940)
The CSM HUD PR caused some strange behavior including aborts due to parts of it using some slightly hacky code, the event refactor changing how events are processed and a minor oversight.

src/game.cpp
@@ -2562,12 +2562,12 @@ void Game::handleClientEvent_HandleParticleEvent(ClientEvent *event,
 void Game::handleClientEvent_HudAdd(ClientEvent *event, CameraOrientation *cam)
 {
 	LocalPlayer *player = client->getEnv().getLocalPlayer();
+	auto hud_server_to_client = client->getHUDTranslationMap();
 
-	u32 id = event->hudadd.id;
-
-	HudElement *e = player->getHud(id);
-
-	if (e != NULL) {
+	u32 server_id = event->hudadd.server_id;
+	// ignore if we already have a HUD with that ID
+	auto i = hud_server_to_client.find(server_id);
+	if (i != hud_server_to_client.end()) {
 		delete event->hudadd.pos;
 		delete event->hudadd.name;
 		delete event->hudadd.scale;
@@ -2579,7 +2579,7 @@ void Game::handleClientEvent_HudAdd(ClientEvent *event, CameraOrientation *cam)
 		return;
 	}
 
-	e = new HudElement;
+	HudElement *e = new HudElement;
 	e->type   = (HudElementType)event->hudadd.type;
 	e->pos    = *event->hudadd.pos;
 	e->name   = *event->hudadd.name;
@@ -2592,10 +2592,7 @@ void Game::handleClientEvent_HudAdd(ClientEvent *event, CameraOrientation *cam)
 	e->offset = *event->hudadd.offset;
 	e->world_pos = *event->hudadd.world_pos;
 	e->size = *event->hudadd.size;
-
-	u32 new_id = player->addHud(e);
-	//if this isn't true our huds aren't consistent
-	sanity_check(new_id == id);
+	hud_server_to_client[server_id] = player->addHud(e);
 
 	delete event->hudadd.pos;
 	delete event->hudadd.name;

src/client.h
@@ -422,6 +422,11 @@ public:
 		return m_csm_noderange_limit;
 	}
 
+	inline std::unordered_map<u32, u32> &getHUDTranslationMap()
+	{
+		return m_hud_server_to_client;
+	}
+
 	bool joinModChannel(const std::string &channel);
 	bool leaveModChannel(const std::string &channel);
 	bool sendModChannelMessage(const std::string &channel, const std::string &message);

src/client/clientevent.h
@@ -116,7 +116,7 @@ struct ClientEvent
 		} delete_particlespawner;
 		struct
 		{
-			u32 id;
+			u32 server_id;
 			u8 type;
 			v2f *pos;
 			std::string *name;

src/network/clientpackethandler.cpp
@@ -1062,12 +1062,10 @@ void Client::handleCommand_HudAdd(NetworkPacket* pkt)
 	try {
 		*pkt >> size;
 	} catch(SerializationError &e) {};
-	u32 client_id = getEnv().getLocalPlayer()->getFreeHudID();
-	m_hud_server_to_client[server_id] = client_id;
 
 	ClientEvent *event = new ClientEvent();
 	event->type             = CE_HUDADD;
-	event->hudadd.id        = client_id;
+	event->hudadd.server_id = server_id;
 	event->hudadd.type      = type;
 	event->hudadd.pos       = new v2f(pos);
 	event->hudadd.name      = new std::string(name);
@@ -1089,7 +1087,7 @@ void Client::handleCommand_HudRemove(NetworkPacket* pkt)
 
 	*pkt >> server_id;
 
-	std::unordered_map<u32, u32>::const_iterator i = m_hud_server_to_client.find(server_id);
+	auto i = m_hud_server_to_client.find(server_id);
 	if (i != m_hud_server_to_client.end()) {
 		int client_id = i->second;
 		m_hud_server_to_client.erase(i);

commit bc979f650ef99f9c7b33bf4be85290e9e8bc7c9b

[CSM] HUD Fix not updating server HUDs caused by 2181fc2

src/game.cpp
@@ -2562,7 +2562,7 @@ void Game::handleClientEvent_HandleParticleEvent(ClientEvent *event,
 void Game::handleClientEvent_HudAdd(ClientEvent *event, CameraOrientation *cam)
 {
 	LocalPlayer *player = client->getEnv().getLocalPlayer();
-	auto hud_server_to_client = client->getHUDTranslationMap();
+	auto &hud_server_to_client = client->getHUDTranslationMap();
 
 	u32 server_id = event->hudadd.server_id;
 	// ignore if we already have a HUD with that ID