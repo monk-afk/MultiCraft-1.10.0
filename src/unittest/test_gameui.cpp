monk-afk/MultiCraft-1.10.0

commit a010749f0fa09ddc9f7ef890268a03ae83fb7ff0

Fix more GCC 8.1 warnings                                                           1   master 
Fix 3 warnings reported by GCC 8.1 of the following type

```src/client/gameui.cpp:191:43: warning: « void* memset(void*, int, size_t) » effacement d'un objet du type non trivial « struct GameUI::Flags »; use assignment or value-initialization instead [-Wclass-memaccess]
  memset(&m_flags, 0, sizeof(GameUI::Flags));
```

src/client/gameui.cpp
@@ -188,7 +188,7 @@ void GameUI::update(const RunStats &stats, Client *client, MapDrawControl *draw_
 
 void GameUI::initFlags()
 {
-	memset(&m_flags, 0, sizeof(GameUI::Flags));
+	m_flags = GameUI::Flags();
 	m_flags.show_chat = true;
 	m_flags.show_hud = true;
 	m_flags.show_debug = g_settings->getBool("show_debug");

src/game.cpp
@@ -1014,7 +1014,8 @@ bool Game::startup(bool *kill,
 	RenderingEngine::get_scene_manager()->getParameters()->
 		setAttribute(scene::OBJ_LOADER_IGNORE_MATERIAL_FILES, true);
 
-	memset(&runData, 0, sizeof(runData));
+	// Reinit runData
+	runData = GameRunData();
 	runData.time_from_last_punch = 10.0;
 	runData.update_wielded_item_trigger = true;
 

src/gui/guiTable.cpp
@@ -334,7 +334,6 @@ void GUITable::setTable(const TableOptions &options,
 
 		// Make template for new cells
 		Cell newcell;
-		memset(&newcell, 0, sizeof newcell);
 		newcell.content_type = columntype;
 		newcell.tooltip_index = tooltip_index;
 		newcell.reported_column = j+1;

commit af841ee2b86b5da27f71892796041f0b36172a0e

Fix GameUI flag value regression introduced by a010749f0fa09ddc9f7ef890268a03ae83fb7ff0
Added more strong unittests on that part to prevent future regression

src/client/gameui.cpp
@@ -189,8 +189,6 @@ void GameUI::update(const RunStats &stats, Client *client, MapDrawControl *draw_
 void GameUI::initFlags()
 {
 	m_flags = GameUI::Flags();
-	m_flags.show_chat = true;
-	m_flags.show_hud = true;
 	m_flags.show_debug = g_settings->getBool("show_debug");
 }
 

src/client/gameui.h
@@ -54,9 +54,9 @@ public:
 	{
 		bool show_chat = true;
 		bool show_hud = true;
-		bool show_minimap = true;
+		bool show_minimap = false;
 		bool show_debug = true;
-		bool show_profiler_graph = true;
+		bool show_profiler_graph = false;
 	};
 
 	void init();

src/unittest/test_gameui.cpp
@@ -48,9 +48,18 @@ void TestGameUI::runTests(IGameDef *gamedef)
 void TestGameUI::testInit()
 {
 	GameUI gui{};
+	// Ensure flags on GameUI init
+	UASSERT(gui.getFlags().show_chat)
+	UASSERT(gui.getFlags().show_hud)
+	UASSERT(!gui.getFlags().show_minimap)
+	UASSERT(!gui.getFlags().show_profiler_graph)
+	
+	// And after the initFlags init stage
 	gui.initFlags();
 	UASSERT(gui.getFlags().show_chat)
 	UASSERT(gui.getFlags().show_hud)
+	UASSERT(!gui.getFlags().show_minimap)
+	UASSERT(!gui.getFlags().show_profiler_graph)
 
 	// @TODO verify if we can create non UI nulldevice to test this function
 	// gui.init();