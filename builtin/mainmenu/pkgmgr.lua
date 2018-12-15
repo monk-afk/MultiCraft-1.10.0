monk-afk/MultiCraft-1.10.0

commit f87b41f9331b429e89ee82ea9d5b77d490fc8aeb

Improve world configure menu (#7922)
Replace `Subgame mods` with <game name> mods.
Display game description on left instead of dependencies.
Fix double DIR_DELIM in some paths.

builtin/mainmenu/pkgmgr.lua
@@ -21,7 +21,7 @@ function get_mods(path,retval,modpack)
 
 	for _, name in ipairs(mods) do
 		if name:sub(1, 1) ~= "." then
-			local prefix = path .. DIR_DELIM .. name .. DIR_DELIM
+			local prefix = path .. DIR_DELIM .. name
 			local toadd = {}
 			retval[#retval + 1] = toadd
 
@@ -39,7 +39,7 @@ function get_mods(path,retval,modpack)
 			if modpack ~= nil and modpack ~= "" then
 				toadd.modpack = modpack
 			else
-				local modpackfile = io.open(prefix .. "modpack.txt")
+				local modpackfile = io.open(prefix .. DIR_DELIM .. "modpack.txt")
 				if modpackfile then
 					modpackfile:close()
 					toadd.type = "modpack"
@@ -532,7 +532,8 @@ function pkgmgr.preparemodlist(data)
 		retval[#retval + 1] = {
 			type = "game",
 			is_game_content = true,
-			name = fgettext("Subgame Mods")
+			name = fgettext(gamespec.name .. " mods"),
+			path = gamespec.path
 		}
 	end
 

builtin/mainmenu/dlg_config_world.lua
@@ -33,11 +33,15 @@ local function get_formspec(data)
 
 	local hard_deps, soft_deps = pkgmgr.get_dependencies(mod.path)
 
-	if mod.is_modpack then
+	if mod.is_modpack or mod.type == "game" then
 		local info = minetest.formspec_escape(
 			core.get_content_info(mod.path).description)
 		if info == "" then
-			info = fgettext("No modpack description provided.")
+			if mod.is_modpack then
+				info = fgettext("No modpack description provided.")
+			else
+				info = fgettext("No game description provided.")
+			end
 		end
 		retval = retval ..
 			"textarea[0.25,0.7;5.75,7.2;;" .. info .. ";]"
@@ -63,12 +67,12 @@ local function get_formspec(data)
 
 			if pkgmgr.is_modpack_entirely_enabled(data, mod.name) then
 				retval = retval ..
-					"button[5.5,0.125;2.5,0.5;btn_mp_disable;" ..
-					fgettext("Disable MP") .. "]"
+					"button[5.5,0.125;3,0.5;btn_mp_disable;" ..
+					fgettext("Disable modpack") .. "]"
 			else
 				retval = retval ..
-					"button[5.5,0.125;2.5,0.5;btn_mp_enable;" ..
-					fgettext("Enable MP") .. "]"
+					"button[5.5,0.125;3,0.5;btn_mp_enable;" ..
+					fgettext("Enable modpack") .. "]"
 			end
 		else
 			retval = retval ..
@@ -78,11 +82,11 @@ local function get_formspec(data)
 	end
 	if enabled_all then
 		retval = retval ..
-			"button[8.75,0.125;2.5,0.5;btn_disable_all_mods;" ..
+			"button[8.95,0.125;2.5,0.5;btn_disable_all_mods;" ..
 			fgettext("Disable all") .. "]"
 	else
 		retval = retval ..
-			"button[8.75,0.125;2.5,0.5;btn_enable_all_mods;" ..
+			"button[8.95,0.125;2.5,0.5;btn_enable_all_mods;" ..
 			fgettext("Enable all") .. "]"
 	end
 	return retval ..

commit 3d53bdaffcefff96c6d388a48b51e8876f79e0d0

Fix ignored mod.conf mod name for world.mt (broken by f87b41f)
Patch supplied by @pgimeno. Thank you.

builtin/mainmenu/pkgmgr.lua
@@ -25,7 +25,7 @@ function get_mods(path,retval,modpack)
 			local toadd = {}
 			retval[#retval + 1] = toadd
 
-			local mod_conf = Settings(prefix .. "mod.conf"):to_table()
+			local mod_conf = Settings(prefix .. DIR_DELIM .. "mod.conf"):to_table()
 			if mod_conf.name then
 				name = mod_conf.name
 			end