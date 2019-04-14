monk-afk/MultiCraft-1.10.0

commit c5e224541d2b0d433892fdbb91d6f52374c11d64

CAO/SAO: Nicer velocity-controlled, interpolated rotation property:
'automatic_face_movement_max_rotation_per_sec'.
Rotate towards the smaller angle.

src/content_sao.cpp
@@ -372,20 +372,20 @@ void LuaEntitySAO::step(float dtime, bool send_recommended)
 			m_velocity += dtime * m_acceleration;
 		}
 
-		if((m_prop.automatic_face_movement_dir) &&
-				(fabs(m_velocity.Z) > 0.001 || fabs(m_velocity.X) > 0.001))
-		{
-			float optimal_yaw = atan2(m_velocity.Z,m_velocity.X) * 180 / M_PI
-					+ m_prop.automatic_face_movement_dir_offset;
+		if (m_prop.automatic_face_movement_dir &&
+				(fabs(m_velocity.Z) > 0.001 || fabs(m_velocity.X) > 0.001)) {
+
+			float target_yaw = atan2(m_velocity.Z, m_velocity.X) * 180 / M_PI
+				+ m_prop.automatic_face_movement_dir_offset;
 			float max_rotation_delta =
 					dtime * m_prop.automatic_face_movement_max_rotation_per_sec;
+			float delta = wrapDegrees_0_360(target_yaw - m_yaw);
 
-			if ((m_prop.automatic_face_movement_max_rotation_per_sec > 0) &&
-				(fabs(m_yaw - optimal_yaw) > max_rotation_delta)) {
-
-				m_yaw = optimal_yaw < m_yaw ? m_yaw - max_rotation_delta : m_yaw + max_rotation_delta;
+			if (delta > max_rotation_delta && 360 - delta > max_rotation_delta) {
+				m_yaw += (delta < 180) ? max_rotation_delta : -max_rotation_delta;
+				m_yaw = wrapDegrees_0_360(m_yaw);
 			} else {
-				m_yaw = optimal_yaw;
+				m_yaw = target_yaw;
 			}
 		}
 	}

src/content_cao.cpp
@@ -904,19 +904,19 @@ void GenericCAO::step(float dtime, ClientEnvironment *env)
 	}
 
 	if (!getParent() && m_prop.automatic_face_movement_dir &&
-			(fabs(m_velocity.Z) > 0.001 || fabs(m_velocity.X) > 0.001))
-	{
-		float optimal_yaw = atan2(m_velocity.Z,m_velocity.X) * 180 / M_PI
+			(fabs(m_velocity.Z) > 0.001 || fabs(m_velocity.X) > 0.001)) {
+
+		float target_yaw = atan2(m_velocity.Z, m_velocity.X) * 180 / M_PI
 				+ m_prop.automatic_face_movement_dir_offset;
 		float max_rotation_delta =
 				dtime * m_prop.automatic_face_movement_max_rotation_per_sec;
+		float delta = wrapDegrees_0_360(target_yaw - m_yaw);
 
-		if ((m_prop.automatic_face_movement_max_rotation_per_sec > 0) &&
-			(fabs(m_yaw - optimal_yaw) > max_rotation_delta)) {
-
-			m_yaw = optimal_yaw < m_yaw ? m_yaw - max_rotation_delta : m_yaw + max_rotation_delta;
+		if (delta > max_rotation_delta && 360 - delta > max_rotation_delta) {
+			m_yaw += (delta < 180) ? max_rotation_delta : -max_rotation_delta;
+			m_yaw = wrapDegrees_0_360(m_yaw);
 		} else {
-			m_yaw = optimal_yaw;
+			m_yaw = target_yaw;
 		}
 		updateNodePos();
 	}

commit b11a7aadccf2c5848bfe7f2a14a4416153aa54b9

Fix regression in automatic_face_movement_max_rotation_per_sec
Values <= 0 should make the yaw change instant. This worked in 0.4.16 but was broken in c5e224541d2b.

Per bug report by oil_boi_minetest on IRC.

src/client/content_cao.cpp
@@ -1000,10 +1000,16 @@ void GenericCAO::step(float dtime, ClientEnvironment *env)
 
 		float target_yaw = atan2(m_velocity.Z, m_velocity.X) * 180 / M_PI
 				+ m_prop.automatic_face_movement_dir_offset;
-		float max_rotation_delta =
-				dtime * m_prop.automatic_face_movement_max_rotation_per_sec;
+		float max_rotation_per_sec =
+				m_prop.automatic_face_movement_max_rotation_per_sec;
+		if (max_rotation_per_sec > 0) {
+			float max_rotation_delta = dtime * max_rotation_per_sec;
 
-		wrappedApproachShortest(m_rotation.Y, target_yaw, max_rotation_delta, 360.f);
+			wrappedApproachShortest(m_rotation.Y, target_yaw, max_rotation_delta, 360.f);
+		} else
+			// Negative values of ...max_rotation_per_sec mean disabled.
+			m_rotation.Y = target_yaw;
+
 		rot_translator.val_current = m_rotation;
 
 		updateNodePos();

src/content_sao.cpp
@@ -457,11 +457,16 @@ void LuaEntitySAO::step(float dtime, bool send_recommended)
 
 			float target_yaw = atan2(m_velocity.Z, m_velocity.X) * 180 / M_PI
 				+ m_prop.automatic_face_movement_dir_offset;
-			float max_rotation_delta =
-					dtime * m_prop.automatic_face_movement_max_rotation_per_sec;
 
-			m_rotation.Y = wrapDegrees_0_360(m_rotation.Y);
-			wrappedApproachShortest(m_rotation.Y, target_yaw, max_rotation_delta, 360.f);
+			float max_rotation_per_sec =
+					m_prop.automatic_face_movement_max_rotation_per_sec;
+			if (max_rotation_per_sec > 0) {
+				float max_rotation_delta = dtime * max_rotation_per_sec;
+				m_rotation.Y = wrapDegrees_0_360(m_rotation.Y);
+				wrappedApproachShortest(m_rotation.Y, target_yaw, max_rotation_delta, 360.f);
+			} else
+				// Negative values of ...max_rotation_per_sec mean disabled.
+				m_rotation.Y = target_yaw;
 		}
 	}
 