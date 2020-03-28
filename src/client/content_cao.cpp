monk-afk/MultiCraft-1.10.0

commit 28afae8ff4d9dd0c3ff2c53b0350130e475e5771

Fix incorrect light updates for wielditem and item visuals (#9540)
An alternative to #9537

src/client/content_cao.cpp
@@ -826,7 +826,11 @@ void GenericCAO::setNodeLight(u8 light)
 {
 	video::SColor color(255, light, light, light);
 
-	if (m_enable_shaders) {
+	if (m_prop.visual == "wielditem" || m_prop.visual == "item") {
+		// Since these types of visuals are using their own shader
+		// they should be handled separately
+		m_wield_meshnode->setColor(color);
+	} else if (m_enable_shaders) {
 		scene::ISceneNode *node = getSceneNode();
 
 		if (node == nullptr)
@@ -850,8 +854,6 @@ void GenericCAO::setNodeLight(u8 light)
 			setMeshColor(m_meshnode->getMesh(), color);
 		} else if (m_animated_meshnode) {
 			setAnimatedMeshColor(m_animated_meshnode, color);
-		} else if (m_wield_meshnode) {
-			m_wield_meshnode->setColor(color);
 		} else if (m_spritenode) {
 			m_spritenode->setColor(color);
 		}

commit aeb7038db6815a96ccd0af097dfa2fdd09a29ff1

Fix crash on wielditem/item entities
bug was introduced in 28afae8ff4d9dd0c3ff2c53b0350130e475e5771

src/client/content_cao.cpp
@@ -829,7 +829,8 @@ void GenericCAO::setNodeLight(u8 light)
 	if (m_prop.visual == "wielditem" || m_prop.visual == "item") {
 		// Since these types of visuals are using their own shader
 		// they should be handled separately
-		m_wield_meshnode->setColor(color);
+		if (m_wield_meshnode)
+			m_wield_meshnode->setColor(color);
 	} else if (m_enable_shaders) {
 		scene::ISceneNode *node = getSceneNode();
 