monk-afk/MultiCraft-1.10.0

commit 7f094e25a9ecd2f3032c56d18cec7c91b1459c36

Settings: Fix game minetest.conf flags overriding defaults (#9404)
The game minetest.conf flags directly overwrote the global minetest.conf default values, resulting in unwanted erased mapgen flags.

* Fix set_mapgen_setting

src/map_settings_manager.cpp
@@ -32,6 +32,7 @@ MapSettingsManager::MapSettingsManager(Settings *user_settings,
 	m_user_settings(user_settings)
 {
 	assert(m_user_settings != NULL);
+	Mapgen::setDefaultSettings(m_map_settings);
 }
 
 

src/content/subgames.cpp
@@ -23,7 +23,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "settings.h"
 #include "log.h"
 #include "util/strfnd.h"
-#include "defaultsettings.h" // for override_default_settings
+#include "defaultsettings.h" // for set_default_settings
 #include "mapgen/mapgen.h"   // for MapgenParams
 #include "util/string.h"
 
@@ -298,7 +298,7 @@ bool loadGameConfAndInitWorld(const std::string &path, const SubgameSpec &gamesp
 	set_default_settings(g_settings);
 	Settings game_defaults;
 	getGameMinetestConfig(gamespec.path, game_defaults);
-	override_default_settings(g_settings, &game_defaults);
+	g_settings->overrideDefaults(&game_defaults);
 
 	infostream << "Initializing world at " << path << std::endl;
 

src/defaultsettings.cpp
@@ -498,10 +498,3 @@ void set_default_settings(Settings *settings)
 #endif
 }
 
-void override_default_settings(Settings *settings, Settings *from)
-{
-	std::vector<std::string> names = from->getNames();
-	for (const auto &name : names) {
-		settings->setDefault(name, from->get(name));
-	}
-}

src/settings.cpp
@@ -1048,6 +1048,30 @@ void Settings::setDefault(const std::string &name, const FlagDesc *flagdesc,
 	setDefault(name, writeFlagString(flags, flagdesc, U32_MAX));
 }
 
+void Settings::overrideDefaults(Settings *other)
+{
+	for (const auto &setting : other->m_settings) {
+		if (setting.second.is_group) {
+			setGroupDefault(setting.first, setting.second.group);
+			continue;
+		}
+		const FlagDesc *flagdesc = getFlagDescFallback(setting.first);
+		if (flagdesc) {
+			// Flags cannot be copied directly.
+			// 1) Get the current set flags
+			u32 flags = getFlagStr(setting.first, flagdesc, nullptr);
+			// 2) Set the flags as defaults
+			other->setDefault(setting.first, flagdesc, flags);
+			// 3) Get the newly set flags and override the default setting value
+			setDefault(setting.first, flagdesc,
+				other->getFlagStr(setting.first, flagdesc, nullptr));
+			continue;
+		}
+		// Also covers FlagDesc settings
+		setDefault(setting.first, setting.second.value);
+	}
+}
+
 const FlagDesc *Settings::getFlagDescFallback(const std::string &name) const
 {
 	auto it = m_flags.find(name);

src/settings.h
@@ -222,6 +222,8 @@ public:
 	 **************/
 
 	void setDefault(const std::string &name, const FlagDesc *flagdesc, u32 flags);
+	// Takes the provided setting values and uses them as new defaults
+	void overrideDefaults(Settings *other);
 	const FlagDesc *getFlagDescFallback(const std::string &name) const;
 
 	void registerChangedCallback(const std::string &name,

commit 33b17828260290d59dacd50ea9b39b49986d61e7

Fix mapgen settings in minetest.conf being ignored (#9737)
broken since 7f094e25a9ecd2f3032c56d18cec7c91b1459c36

src/map_settings_manager.cpp
@@ -32,7 +32,6 @@ MapSettingsManager::MapSettingsManager(Settings *user_settings,
 	m_user_settings(user_settings)
 {
 	assert(m_user_settings != NULL);
-	Mapgen::setDefaultSettings(m_map_settings);
 }
 
 
@@ -179,6 +178,16 @@ MapgenParams *MapSettingsManager::makeMapgenParams()
 		return nullptr;
 
 	params->mgtype = mgtype;
+
+	// Load the mapgen param defaults
+	/* FIXME: Why is it done like this? MapgenParams should just
+	 * set the defaults in its constructor instead. */
+	{
+		Settings default_settings;
+		Mapgen::setDefaultSettings(&default_settings);
+		params->MapgenParams::readParams(&default_settings);
+		params->readParams(&default_settings);
+	}
 
 	// Load the rest of the mapgen params from our active settings
 	params->MapgenParams::readParams(m_user_settings);