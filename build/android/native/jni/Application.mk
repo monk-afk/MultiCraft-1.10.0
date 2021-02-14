mightyjoe781/MultiCraft2

Backport MultiCraft Android Java part

build/android/native/jni/Application.mk
@@ -1,11 +1,11 @@
 APP_PLATFORM := ${APP_PLATFORM}
 APP_ABI := ${TARGET_ABI}
-APP_STL := c++_shared
+APP_STL := c++_static
 NDK_TOOLCHAIN_VERSION := clang
 APP_SHORT_COMMANDS := true
-APP_MODULES := Minetest
+APP_MODULES := MultiCraft
 
-APP_CPPFLAGS := -Ofast -fvisibility=hidden -fexceptions -Wno-deprecated-declarations -Wno-extra-tokens
+APP_CPPFLAGS := -Ofast -fvisibility=hidden -Wno-extra-tokens
 
 ifeq ($(APP_ABI),armeabi-v7a)
 APP_CPPFLAGS += -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb
@@ -16,17 +16,23 @@ endif
 #endif
 
 ifndef NDEBUG
-APP_CPPFLAGS := -g -D_DEBUG -O0 -fno-omit-frame-pointer -fexceptions
+APP_CPPFLAGS := -g -D_DEBUG -O0 -fno-omit-frame-pointer
 endif
 
-APP_CFLAGS   := $(APP_CPPFLAGS) -Wno-parentheses-equality #-Werror=shorten-64-to-32
+APP_CPPFLAGS += -fexceptions #-Werror=shorten-64-to-32
+
+# Silence Irrlicht warnings. Comment out with real debugging!
+APP_CPPFLAGS += -Wno-deprecated-declarations -Wno-inconsistent-missing-override
+
+APP_CFLAGS   := $(APP_CPPFLAGS)
 APP_CXXFLAGS := $(APP_CPPFLAGS) -frtti -std=gnu++17
-APP_LDFLAGS  := -Wl,--no-warn-mismatch,--gc-sections,--icf=safe
+
+ifdef NDEBUG
+APP_LDFLAGS  := -Wl,--gc-sections,--icf=all
+else
+APP_LDFLAGS  :=
+endif
 
 ifeq ($(APP_ABI),arm64-v8a)
-APP_LDFLAGS  := -Wl,--no-warn-mismatch,--gc-sections
-endif
-
-ifndef NDEBUG
-APP_LDFLAGS  :=
+APP_LDFLAGS  += -fuse-ld=gold
 endif

monk-afk/MultiCraft-1.10.0

build/android/native/jni/Application.mk
@@ -1,11 +1,11 @@
 APP_PLATFORM := ${APP_PLATFORM}
 APP_ABI := ${TARGET_ABI}
-APP_STL := c++_shared
+APP_STL := c++_static
 NDK_TOOLCHAIN_VERSION := clang
 APP_SHORT_COMMANDS := true
-APP_MODULES := Minetest
+APP_MODULES := MultiCraft
 
-APP_CPPFLAGS := -Ofast -fvisibility=hidden -fexceptions -Wno-deprecated-declarations -Wno-extra-tokens
+APP_CPPFLAGS := -Ofast -fvisibility=hidden -Wno-extra-tokens
 
 ifeq ($(APP_ABI),armeabi-v7a)
 APP_CPPFLAGS += -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb
@@ -16,17 +16,21 @@ endif
 #endif
 
 ifndef NDEBUG
-APP_CPPFLAGS := -g -D_DEBUG -O0 -fno-omit-frame-pointer -fexceptions
+APP_CPPFLAGS := -g -D_DEBUG -O0 -fno-omit-frame-pointer
 endif
 
-APP_CFLAGS   := $(APP_CPPFLAGS) -Wno-parentheses-equality #-Werror=shorten-64-to-32
+APP_CPPFLAGS += -fexceptions #-Werror=shorten-64-to-32
+
+# Silence Irrlicht warnings. Comment out with real debugging!
+APP_CPPFLAGS += -Wno-deprecated-declarations -Wno-inconsistent-missing-override
+
+APP_CFLAGS   := $(APP_CPPFLAGS)
 APP_CXXFLAGS := $(APP_CPPFLAGS) -frtti -std=gnu++17
-APP_LDFLAGS  := -Wl,--no-warn-mismatch,--gc-sections,--icf=safe
 
-ifeq ($(APP_ABI),arm64-v8a)
-APP_LDFLAGS  := -Wl,--no-warn-mismatch,--gc-sections
-endif
-
-ifndef NDEBUG
+ifdef NDEBUG
+APP_LDFLAGS  := -Wl,--gc-sections,--icf=all
+else
 APP_LDFLAGS  :=
 endif
+
+APP_LDFLAGS  += -fuse-ld=lld