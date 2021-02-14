MultiCraft/MultiCraft

Backport MultiCraft Engine changes

src/client/renderingengine.cpp
@@ -38,7 +38,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "../gui/guiSkin.h"
 
 #if !defined(_WIN32) && !defined(__APPLE__) && !defined(__ANDROID__) && \
-		!defined(SERVER) && !defined(__HAIKU__)
+		!defined(SERVER) && !defined(__HAIKU__) && !defined(__IOS__)
 #define XORG_USED
 #endif
 #ifdef XORG_USED
@@ -85,6 +85,12 @@ RenderingEngine::RenderingEngine(IEventReceiver *receiver)
 {
 	sanity_check(!s_singleton);
 
+#ifdef __ANDROID__
+	// Set correct resolution
+	g_settings->setU16("screen_w", porting::getDisplaySize().X);
+	g_settings->setU16("screen_h", porting::getDisplaySize().Y);
+#endif
+
 	// Resolution selection
 	bool fullscreen = g_settings->getBool("fullscreen");
 	u16 screen_w = g_settings->getU16("screen_w");
@@ -129,7 +135,7 @@ RenderingEngine::RenderingEngine(IEventReceiver *receiver)
 	params.EventReceiver = receiver;
 	params.HighPrecisionFPU = g_settings->getBool("high_precision_fpu");
 	params.ZBufferBits = 24;
-#ifdef __ANDROID__
+#if defined(__ANDROID__) || defined(__IOS__)
 	params.PrivateData = porting::app_global;
 #endif
 #if ENABLE_GLES
@@ -148,6 +154,16 @@ RenderingEngine::RenderingEngine(IEventReceiver *receiver)
 			gui::EGST_WINDOWS_METALLIC, driver);
 	m_device->getGUIEnvironment()->setSkin(skin);
 	skin->drop();
+
+#ifdef __IOS__
+	if (device) {
+		CIrrDeviceiOS* dev = (CIrrDeviceiOS*) device;
+		porting::setViewController(dev->getViewController());
+#ifdef ADS
+		ads_startup(dev->getViewController());
+#endif
+	}
+#endif
 }
 
 RenderingEngine::~RenderingEngine()
@@ -475,6 +491,11 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 		gui::IGUIEnvironment *guienv, ITextureSource *tsrc, float dtime,
 		int percent, bool clouds)
 {
+#ifdef __IOS__
+	if (device->isWindowMinimized())
+		return;
+#endif
+
 	v2u32 screensize = RenderingEngine::get_instance()->getWindowSize();
 
 	v2s32 textsize(g_fontengine->getTextWidth(text), g_fontengine->getLineHeight());
@@ -485,6 +506,7 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 			guienv->addStaticText(text.c_str(), textrect, false, false);
 	guitext->setTextAlignment(gui::EGUIA_CENTER, gui::EGUIA_UPPERLEFT);
 
+	clouds = false; // disable clouds at loading time
 	bool cloud_menu_background = clouds && g_settings->getBool("menu_clouds");
 	if (cloud_menu_background) {
 		g_menuclouds->step(dtime * 3);
@@ -492,8 +514,14 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 		get_video_driver()->beginScene(
 				true, true, video::SColor(255, 140, 186, 250));
 		g_menucloudsmgr->drawAll();
-	} else
+	} else {
 		get_video_driver()->beginScene(true, true, video::SColor(255, 0, 0, 0));
+		video::ITexture *background_image = tsrc->getTexture("bg.png");
+
+		get_video_driver()->draw2DImage(background_image,
+			irr::core::rect<s32>(0, 0, screensize.X * 4, screensize.Y * 4),
+			irr::core::rect<s32>(0, 0, screensize.X, screensize.Y), 0, 0, false);
+	}
 
 	// draw progress bar
 	if ((percent >= 0) && (percent <= 100)) {
@@ -502,27 +530,76 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 				tsrc->getTexture("progress_bar_bg.png");
 
 		if (progress_img && progress_img_bg) {
-#ifndef __ANDROID__
 			const core::dimension2d<u32> &img_size =
 					progress_img_bg->getSize();
-			u32 imgW = rangelim(img_size.Width, 200, 600);
-			u32 imgH = rangelim(img_size.Height, 24, 72);
+#if !defined(__ANDROID__) && !defined(__IOS__)
+			u32 imgW = rangelim(img_size.Width, 256, 1024);
+			u32 imgH = rangelim(img_size.Height, 32, 128);
+			float imgR = 1.0f;
 #else
-			const core::dimension2d<u32> img_size(256, 48);
-			float imgRatio = (float)img_size.Height / img_size.Width;
-			u32 imgW = screensize.X / 2.2f;
-			u32 imgH = floor(imgW * imgRatio);
+			float imgRatio = (float) img_size.Height / img_size.Width;
+			u32 imgW = npot2(screensize.X / 2.0f);
+			if (imgW > (screensize.X * 0.6) && imgW >= 1024)
+				imgW /= 2;
+			u32 imgH = imgW * imgRatio;
+			float imgR = (float) (imgW) / img_size.Width;
 #endif
 			v2s32 img_pos((screensize.X - imgW) / 2,
 					(screensize.Y - imgH) / 2);
 
-			draw2DImageFilterScaled(get_video_driver(), progress_img_bg,
-					core::rect<s32>(img_pos.X, img_pos.Y,
-							img_pos.X + imgW,
-							img_pos.Y + imgH),
-					core::rect<s32>(0, 0, img_size.Width,
-							img_size.Height),
-					0, 0, true);
+			draw2DImageFilterScaled(
+				driver, progress_img_bg,
+				core::rect<s32>(img_pos.X, img_pos.Y, img_pos.X + imgW, img_pos.Y + imgH),
+				core::rect<s32>(0, 0, img_size.Width, img_size.Height),
+				0, 0, true);
+
+			// rects for drawing a color progress bar
+			const static core::rect<s32> rects[] = {
+				core::rect<s32>(  4, 24,   5, 40),
+				core::rect<s32>(  5, 21,   6, 43),
+				core::rect<s32>(  6, 19,   7, 45),
+				core::rect<s32>(  7, 17,   8, 47),
+				core::rect<s32>(  8, 15,   9, 49),
+				core::rect<s32>(  9, 14,  10, 50),
+				core::rect<s32>( 10, 13,  11, 51),
+				core::rect<s32>( 11, 12,  12, 52),
+				core::rect<s32>( 12, 11,  13, 53),
+				core::rect<s32>( 13, 10,  14, 54),
+				core::rect<s32>( 14,  9,  15, 55),
+				core::rect<s32>( 15,  8,  17, 56),
+				core::rect<s32>( 17,  7,  19, 57),
+				core::rect<s32>( 19,  6,  21, 58),
+				core::rect<s32>( 21,  5,  24, 59),
+				core::rect<s32>( 24,  4, 488, 60),
+				core::rect<s32>(488,  5, 491, 59),
+				core::rect<s32>(491,  6, 493, 58),
+				core::rect<s32>(493,  7, 495, 57),
+				core::rect<s32>(495,  8, 497, 56),
+				core::rect<s32>(497,  9, 498, 55),
+				core::rect<s32>(498, 10, 499, 54),
+				core::rect<s32>(499, 11, 500, 53),
+				core::rect<s32>(500, 12, 501, 52),
+				core::rect<s32>(501, 13, 502, 51),
+				core::rect<s32>(502, 14, 503, 50),
+				core::rect<s32>(503, 15, 504, 49),
+				core::rect<s32>(504, 17, 505, 47),
+				core::rect<s32>(505, 19, 506, 45),
+				core::rect<s32>(506, 21, 507, 43),
+				core::rect<s32>(507, 24, 508, 40)
+			};
+
+			for (const auto & i : rects) {
+				const s32 clipx = (percent * imgW) / 100;
+				core::rect<s32> r(
+					MYMIN(i.UpperLeftCorner.X * imgR, clipx), i.UpperLeftCorner.Y * imgR,
+					MYMIN(i.LowerRightCorner.X * imgR, clipx), i.LowerRightCorner.Y * imgR
+				);
+				if (r.getArea() <= 0)
+					break;
+				get_video_driver()->draw2DRectangle(
+					video::SColor(255, 255 - percent * 2, percent * 2, 25),
+					r + img_pos, nullptr);
+			}
 
 			draw2DImageFilterScaled(get_video_driver(), progress_img,
 					core::rect<s32>(img_pos.X, img_pos.Y,
@@ -641,7 +718,7 @@ const char *RenderingEngine::getVideoDriverFriendlyName(irr::video::E_DRIVER_TYP
 	return driver_names[type];
 }
 
-#ifndef __ANDROID__
+#if !defined(__ANDROID__) && !defined(__IOS__)
 #if defined(XORG_USED)
 
 static float calcDisplayDensity()
@@ -725,7 +802,7 @@ v2u32 RenderingEngine::getDisplaySize()
 	return deskres;
 }
 
-#else // __ANDROID__
+#else // __ANDROID__/__IOS__
 float RenderingEngine::getDisplayDensity()
 {
 	return porting::getDisplayDensity();
@@ -735,4 +812,4 @@ v2u32 RenderingEngine::getDisplaySize()
 {
 	return porting::getDisplaySize();
 }
-#endif // __ANDROID__
+#endif // __ANDROID__/__IOS__

src/serverenvironment.cpp
@@ -40,7 +40,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "gameparams.h"
 #include "database/database-dummy.h"
 #include "database/database-files.h"
+#if USE_SQLITE
 #include "database/database-sqlite3.h"
+#endif
 #if USE_POSTGRESQL
 #include "database/database-postgresql.h"
 #endif
@@ -405,8 +407,13 @@ ServerEnvironment::ServerEnvironment(ServerMap *map,
 	std::string conf_path = path_world + DIR_DELIM + "world.mt";
 	Settings conf;
 
+#if !defined(__ANDROID__) && !defined(__IOS__)
 	std::string player_backend_name = "sqlite3";
 	std::string auth_backend_name = "sqlite3";
+#else
+	std::string player_backend_name = "leveldb";
+	std::string auth_backend_name = "leveldb";
+#endif
 
 	bool succeeded = conf.readConfigFile(conf_path.c_str());
 
@@ -444,17 +451,17 @@ ServerEnvironment::ServerEnvironment(ServerMap *map,
 		}
 	}
 
-	if (player_backend_name == "files") {
+	/*if (player_backend_name == "files") {
 		warningstream << "/!\\ You are using old player file backend. "
 				<< "This backend is deprecated and will be removed in a future release /!\\"
 				<< std::endl << "Switching to SQLite3 or PostgreSQL is advised, "
 				<< "please read http://wiki.minetest.net/Database_backends." << std::endl;
-	}
+	}*/
 
 	if (auth_backend_name == "files") {
 		warningstream << "/!\\ You are using old auth file backend. "
 				<< "This backend is deprecated and will be removed in a future release /!\\"
-				<< std::endl << "Switching to SQLite3 is advised, "
+				<< std::endl << "Switching to LevelDB or SQLite3 is advised, "
 				<< "please read http://wiki.minetest.net/Database_backends." << std::endl;
 	}
 
@@ -692,7 +699,7 @@ void ServerEnvironment::loadMeta()
 
 	setTimeOfDay(args.exists("time_of_day") ?
 		// set day to early morning by default
-		args.getU64("time_of_day") : 5250);
+		args.getU64("time_of_day") : 6000);
 
 	m_last_clear_objects_time = args.exists("last_clear_objects_time") ?
 		// If missing, do as if clearObjects was never called
@@ -1818,7 +1825,7 @@ void ServerEnvironment::activateObjects(MapBlock *block, u32 dtime_s)
 		<<" objects)"<<std::endl;
 	bool large_amount = (block->m_static_objects.m_stored.size() > g_settings->getU16("max_objects_per_block"));
 	if (large_amount) {
-		errorstream<<"suspiciously large amount of objects detected: "
+		warningstream<<"suspiciously large amount of objects detected: "
 			<<block->m_static_objects.m_stored.size()<<" in "
 			<<PP(block->getPos())
 			<<"; removing all of them."<<std::endl;
@@ -2078,9 +2085,10 @@ bool ServerEnvironment::saveStaticToBlock(
 PlayerDatabase *ServerEnvironment::openPlayerDatabase(const std::string &name,
 		const std::string &savedir, const Settings &conf)
 {
-
+#if USE_SQLITE
 	if (name == "sqlite3")
 		return new PlayerDatabaseSQLite3(savedir);
+#endif
 
 	if (name == "dummy")
 		return new Database_Dummy();
@@ -2101,7 +2109,7 @@ PlayerDatabase *ServerEnvironment::openPlayerDatabase(const std::string &name,
 	if (name == "files")
 		return new PlayerDatabaseFiles(savedir + DIR_DELIM + "players");
 
-	throw BaseException(std::string("Database backend ") + name + " not supported.");
+	throw ModError(std::string("Database backend ") + name + " not supported.");
 }
 
 bool ServerEnvironment::migratePlayersDatabase(const GameParams &game_params,
@@ -2194,8 +2202,10 @@ bool ServerEnvironment::migratePlayersDatabase(const GameParams &game_params,
 AuthDatabase *ServerEnvironment::openAuthDatabase(
 		const std::string &name, const std::string &savedir, const Settings &conf)
 {
+#if USE_SQLITE
 	if (name == "sqlite3")
 		return new AuthDatabaseSQLite3(savedir);
+#endif
 
 #if USE_POSTGRESQL
 	if (name == "postgresql") {
@@ -2213,7 +2223,7 @@ AuthDatabase *ServerEnvironment::openAuthDatabase(
 		return new AuthDatabaseLevelDB(savedir);
 #endif
 
-	throw BaseException(std::string("Database backend ") + name + " not supported.");
+	throw ModError(std::string("Database backend ") + name + " not supported.");
 }
 
 bool ServerEnvironment::migrateAuthDatabase(

src/util/areastore.h
@@ -27,7 +27,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <istream>
 #include "util/container.h"
 #include "util/numeric.h"
-#ifndef ANDROID
+#if !defined(__ANDROID__) && !defined(__IOS__)
 	#include "cmake_config.h"
 #endif
 #if USE_SPATIAL

monk-afk/MultiCraft-1.10.0

src/client/renderingengine.cpp
@@ -38,7 +38,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "../gui/guiSkin.h"
 
 #if !defined(_WIN32) && !defined(__APPLE__) && !defined(__ANDROID__) && \
-		!defined(SERVER) && !defined(__HAIKU__)
+		!defined(SERVER) && !defined(__HAIKU__) && !defined(__IOS__)
 #define XORG_USED
 #endif
 #ifdef XORG_USED
@@ -85,6 +85,12 @@ RenderingEngine::RenderingEngine(IEventReceiver *receiver)
 {
 	sanity_check(!s_singleton);
 
+#ifdef __ANDROID__
+	// Set correct resolution
+	g_settings->setU16("screen_w", porting::getDisplaySize().X);
+	g_settings->setU16("screen_h", porting::getDisplaySize().Y);
+#endif
+
 	// Resolution selection
 	bool fullscreen = g_settings->getBool("fullscreen");
 	u16 screen_w = g_settings->getU16("screen_w");
@@ -129,7 +135,7 @@ RenderingEngine::RenderingEngine(IEventReceiver *receiver)
 	params.EventReceiver = receiver;
 	params.HighPrecisionFPU = g_settings->getBool("high_precision_fpu");
 	params.ZBufferBits = 24;
-#ifdef __ANDROID__
+#if defined(__ANDROID__) || defined(__IOS__)
 	params.PrivateData = porting::app_global;
 #endif
 #if ENABLE_GLES
@@ -148,6 +154,16 @@ RenderingEngine::RenderingEngine(IEventReceiver *receiver)
 			gui::EGST_WINDOWS_METALLIC, driver);
 	m_device->getGUIEnvironment()->setSkin(skin);
 	skin->drop();
+
+#ifdef __IOS__
+	if (device) {
+		CIrrDeviceiOS* dev = (CIrrDeviceiOS*) device;
+		porting::setViewController(dev->getViewController());
+#ifdef ADS
+		ads_startup(dev->getViewController());
+#endif
+	}
+#endif
 }
 
 RenderingEngine::~RenderingEngine()
@@ -475,6 +491,14 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 		gui::IGUIEnvironment *guienv, ITextureSource *tsrc, float dtime,
 		int percent, bool clouds)
 {
+#ifdef __IOS__
+		if (m_device->isWindowMinimized())
+			return;
+#else
+		if (!m_device->isWindowFocused())
+			return;
+#endif
+
 	v2u32 screensize = RenderingEngine::get_instance()->getWindowSize();
 
 	v2s32 textsize(g_fontengine->getTextWidth(text), g_fontengine->getLineHeight());
@@ -485,6 +509,7 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 			guienv->addStaticText(text.c_str(), textrect, false, false);
 	guitext->setTextAlignment(gui::EGUIA_CENTER, gui::EGUIA_UPPERLEFT);
 
+	clouds = false; // disable clouds at loading time
 	bool cloud_menu_background = clouds && g_settings->getBool("menu_clouds");
 	if (cloud_menu_background) {
 		g_menuclouds->step(dtime * 3);
@@ -492,8 +517,14 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 		get_video_driver()->beginScene(
 				true, true, video::SColor(255, 140, 186, 250));
 		g_menucloudsmgr->drawAll();
-	} else
+	} else {
 		get_video_driver()->beginScene(true, true, video::SColor(255, 0, 0, 0));
+		video::ITexture *background_image = tsrc->getTexture("bg.png");
+
+		get_video_driver()->draw2DImage(background_image,
+			irr::core::rect<s32>(0, 0, screensize.X * 4, screensize.Y * 4),
+			irr::core::rect<s32>(0, 0, screensize.X, screensize.Y), 0, 0, false);
+	}
 
 	// draw progress bar
 	if ((percent >= 0) && (percent <= 100)) {
@@ -502,27 +533,78 @@ void RenderingEngine::_draw_load_screen(const std::wstring &text,
 				tsrc->getTexture("progress_bar_bg.png");
 
 		if (progress_img && progress_img_bg) {
-#ifndef __ANDROID__
 			const core::dimension2d<u32> &img_size =
 					progress_img_bg->getSize();
-			u32 imgW = rangelim(img_size.Width, 200, 600);
-			u32 imgH = rangelim(img_size.Height, 24, 72);
+#if !defined(__ANDROID__) && !defined(__IOS__)
+			float scale = RenderingEngine::getDisplayDensity();
+			scale = scale >= 1 ? scale : 1;
+			u32 imgW = rangelim(img_size.Width, 256, 1024) * scale;
+			u32 imgH = rangelim(img_size.Height, 32, 128) * scale;
+			float imgR = scale;
 #else
-			const core::dimension2d<u32> img_size(256, 48);
-			float imgRatio = (float)img_size.Height / img_size.Width;
-			u32 imgW = screensize.X / 2.2f;
-			u32 imgH = floor(imgW * imgRatio);
+			float imgRatio = (float) img_size.Height / img_size.Width;
+			u32 imgW = npot2(screensize.X / 2.0f);
+			if (imgW > (screensize.X * 0.7) && imgW >= 1024)
+				imgW /= 2;
+			u32 imgH = imgW * imgRatio;
+			float imgR = (float) (imgW) / img_size.Width;
 #endif
 			v2s32 img_pos((screensize.X - imgW) / 2,
 					(screensize.Y - imgH) / 2);
 
-			draw2DImageFilterScaled(get_video_driver(), progress_img_bg,
-					core::rect<s32>(img_pos.X, img_pos.Y,
-							img_pos.X + imgW,
-							img_pos.Y + imgH),
-					core::rect<s32>(0, 0, img_size.Width,
-							img_size.Height),
-					0, 0, true);
+			draw2DImageFilterScaled(
+				driver, progress_img_bg,
+				core::rect<s32>(img_pos.X, img_pos.Y, img_pos.X + imgW, img_pos.Y + imgH),
+				core::rect<s32>(0, 0, img_size.Width, img_size.Height),
+				0, 0, true);
+
+			// rects for drawing a color progress bar
+			const static core::rect<s32> rects[] = {
+				core::rect<s32>(  4, 24,   5, 40),
+				core::rect<s32>(  5, 21,   6, 43),
+				core::rect<s32>(  6, 19,   7, 45),
+				core::rect<s32>(  7, 17,   8, 47),
+				core::rect<s32>(  8, 15,   9, 49),
+				core::rect<s32>(  9, 14,  10, 50),
+				core::rect<s32>( 10, 13,  11, 51),
+				core::rect<s32>( 11, 12,  12, 52),
+				core::rect<s32>( 12, 11,  13, 53),
+				core::rect<s32>( 13, 10,  14, 54),
+				core::rect<s32>( 14,  9,  15, 55),
+				core::rect<s32>( 15,  8,  17, 56),
+				core::rect<s32>( 17,  7,  19, 57),
+				core::rect<s32>( 19,  6,  21, 58),
+				core::rect<s32>( 21,  5,  24, 59),
+				core::rect<s32>( 24,  4, 488, 60),
+				core::rect<s32>(488,  5, 491, 59),
+				core::rect<s32>(491,  6, 493, 58),
+				core::rect<s32>(493,  7, 495, 57),
+				core::rect<s32>(495,  8, 497, 56),
+				core::rect<s32>(497,  9, 498, 55),
+				core::rect<s32>(498, 10, 499, 54),
+				core::rect<s32>(499, 11, 500, 53),
+				core::rect<s32>(500, 12, 501, 52),
+				core::rect<s32>(501, 13, 502, 51),
+				core::rect<s32>(502, 14, 503, 50),
+				core::rect<s32>(503, 15, 504, 49),
+				core::rect<s32>(504, 17, 505, 47),
+				core::rect<s32>(505, 19, 506, 45),
+				core::rect<s32>(506, 21, 507, 43),
+				core::rect<s32>(507, 24, 508, 40)
+			};
+
+			for (const auto & i : rects) {
+				const s32 clipx = (percent * imgW) / 100;
+				core::rect<s32> r(
+					MYMIN(i.UpperLeftCorner.X * imgR, clipx), i.UpperLeftCorner.Y * imgR,
+					MYMIN(i.LowerRightCorner.X * imgR, clipx), i.LowerRightCorner.Y * imgR
+				);
+				if (r.getArea() <= 0)
+					break;
+				get_video_driver()->draw2DRectangle(
+					video::SColor(255, 255 - percent * 2, percent * 2, 25),
+					r + img_pos, nullptr);
+			}
 
 			draw2DImageFilterScaled(get_video_driver(), progress_img,
 					core::rect<s32>(img_pos.X, img_pos.Y,
@@ -641,7 +723,7 @@ const char *RenderingEngine::getVideoDriverFriendlyName(irr::video::E_DRIVER_TYP
 	return driver_names[type];
 }
 
-#ifndef __ANDROID__
+#if !defined(__ANDROID__) && !defined(__IOS__)
 #if defined(XORG_USED)
 
 static float calcDisplayDensity()
@@ -725,7 +807,7 @@ v2u32 RenderingEngine::getDisplaySize()
 	return deskres;
 }
 
-#else // __ANDROID__
+#else // __ANDROID__/__IOS__
 float RenderingEngine::getDisplayDensity()
 {
 	return porting::getDisplayDensity();
@@ -735,4 +817,4 @@ v2u32 RenderingEngine::getDisplaySize()
 {
 	return porting::getDisplaySize();
 }
-#endif // __ANDROID__
+#endif // __ANDROID__/__IOS__

src/serverenvironment.cpp
@@ -40,7 +40,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "gameparams.h"
 #include "database/database-dummy.h"
 #include "database/database-files.h"
+#if USE_SQLITE
 #include "database/database-sqlite3.h"
+#endif
 #if USE_POSTGRESQL
 #include "database/database-postgresql.h"
 #endif
@@ -405,8 +407,13 @@ ServerEnvironment::ServerEnvironment(ServerMap *map,
 	std::string conf_path = path_world + DIR_DELIM + "world.mt";
 	Settings conf;
 
+#if !defined(__ANDROID__) && !defined(__APPLE__)
 	std::string player_backend_name = "sqlite3";
 	std::string auth_backend_name = "sqlite3";
+#else
+	std::string player_backend_name = "leveldb";
+	std::string auth_backend_name = "leveldb";
+#endif
 
 	bool succeeded = conf.readConfigFile(conf_path.c_str());
 
@@ -444,7 +451,7 @@ ServerEnvironment::ServerEnvironment(ServerMap *map,
 		}
 	}
 
-	if (player_backend_name == "files") {
+	/*if (player_backend_name == "files") {
 		warningstream << "/!\\ You are using old player file backend. "
 				<< "This backend is deprecated and will be removed in a future release /!\\"
 				<< std::endl << "Switching to SQLite3 or PostgreSQL is advised, "
@@ -454,9 +461,9 @@ ServerEnvironment::ServerEnvironment(ServerMap *map,
 	if (auth_backend_name == "files") {
 		warningstream << "/!\\ You are using old auth file backend. "
 				<< "This backend is deprecated and will be removed in a future release /!\\"
-				<< std::endl << "Switching to SQLite3 is advised, "
+				<< std::endl << "Switching to LevelDB or SQLite3 is advised, "
 				<< "please read http://wiki.minetest.net/Database_backends." << std::endl;
-	}
+	}*/
 
 	m_player_database = openPlayerDatabase(player_backend_name, path_world, conf);
 	m_auth_database = openAuthDatabase(auth_backend_name, path_world, conf);
@@ -692,7 +699,7 @@ void ServerEnvironment::loadMeta()
 
 	setTimeOfDay(args.exists("time_of_day") ?
 		// set day to early morning by default
-		args.getU64("time_of_day") : 5250);
+		args.getU64("time_of_day") : 6000);
 
 	m_last_clear_objects_time = args.exists("last_clear_objects_time") ?
 		// If missing, do as if clearObjects was never called
@@ -1818,7 +1825,7 @@ void ServerEnvironment::activateObjects(MapBlock *block, u32 dtime_s)
 		<<" objects)"<<std::endl;
 	bool large_amount = (block->m_static_objects.m_stored.size() > g_settings->getU16("max_objects_per_block"));
 	if (large_amount) {
-		errorstream<<"suspiciously large amount of objects detected: "
+		warningstream<<"suspiciously large amount of objects detected: "
 			<<block->m_static_objects.m_stored.size()<<" in "
 			<<PP(block->getPos())
 			<<"; removing all of them."<<std::endl;
@@ -2078,9 +2085,10 @@ bool ServerEnvironment::saveStaticToBlock(
 PlayerDatabase *ServerEnvironment::openPlayerDatabase(const std::string &name,
 		const std::string &savedir, const Settings &conf)
 {
-
+#if USE_SQLITE
 	if (name == "sqlite3")
 		return new PlayerDatabaseSQLite3(savedir);
+#endif
 
 	if (name == "dummy")
 		return new Database_Dummy();
@@ -2101,7 +2109,7 @@ PlayerDatabase *ServerEnvironment::openPlayerDatabase(const std::string &name,
 	if (name == "files")
 		return new PlayerDatabaseFiles(savedir + DIR_DELIM + "players");
 
-	throw BaseException(std::string("Database backend ") + name + " not supported.");
+	throw ModError(std::string("Database backend ") + name + " not supported.");
 }
 
 bool ServerEnvironment::migratePlayersDatabase(const GameParams &game_params,
@@ -2194,8 +2202,10 @@ bool ServerEnvironment::migratePlayersDatabase(const GameParams &game_params,
 AuthDatabase *ServerEnvironment::openAuthDatabase(
 		const std::string &name, const std::string &savedir, const Settings &conf)
 {
+#if USE_SQLITE
 	if (name == "sqlite3")
 		return new AuthDatabaseSQLite3(savedir);
+#endif
 
 #if USE_POSTGRESQL
 	if (name == "postgresql") {
@@ -2213,7 +2223,7 @@ AuthDatabase *ServerEnvironment::openAuthDatabase(
 		return new AuthDatabaseLevelDB(savedir);
 #endif
 
-	throw BaseException(std::string("Database backend ") + name + " not supported.");
+	throw ModError(std::string("Database backend ") + name + " not supported.");
 }
 
 bool ServerEnvironment::migrateAuthDatabase(

src/util/areastore.h
@@ -27,7 +27,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <istream>
 #include "util/container.h"
 #include "util/numeric.h"
-#ifndef ANDROID
+#if !defined(__ANDROID__) && !defined(__APPLE__)
 	#include "cmake_config.h"
 #endif
 #if USE_SPATIAL